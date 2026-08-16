3a1b9c02f5cc06154c9f569e15b74ebd60dae109
kanhanimbus <saket.r.bakshi@gmail.com>
2026-08-16 17:13:19 +0530

    deployment fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: 'https://kanhanimbus.github.io',
+  site: 'https://kanhanimbus.github.io/website',
   base: ''
 });
