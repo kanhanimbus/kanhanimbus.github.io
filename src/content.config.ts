c63a52657b185f46fcb4449ea810aa25952e4437
kanhanimbus <saket.r.bakshi@gmail.com>
2026-09-06 22:32:03 +0530

    added amrita.town link widget

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,7 +17,7 @@ const blogInfluences = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        pubDate: z.date(),
+        pubDate: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
     }),
 });
@@ -27,7 +27,7 @@ const blogLogs = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        pubDate: z.date(),
+        pubDate: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
     }),
 });
@@ -37,7 +37,7 @@ const blogRecs = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        pubDate: z.date(),
+        pubDate: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
     }),
 });
@@ -47,7 +47,7 @@ const blog = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        pubDate: z.date(),
+        pubDate: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
     }),
 });
